7e32ef4139068e8746e94cf1087e2e214df8c35c
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-24 18:28:02 +0300

    MVC

---
+++ src/main/java/com/potinga/spring/mvc/MyController.java
@@ -0,0 +1,12 @@
+package com.potinga.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+    @RequestMapping("/")
+    public String showFIrstView(){
+        return "first-view";
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/first-view.jsp
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<h2> Vezi cit e de Bravo tata? </h2>
+
+</body>
+
+</html>
