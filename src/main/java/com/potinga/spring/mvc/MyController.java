23b9b809394835e7053779d69f0c53bf758aa450
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-25 20:49:50 +0300

    modificari controler

--- src/main/java/com/potinga/spring/mvc/MyController.java
+++ src/main/java/com/potinga/spring/mvc/MyController.java
@@ -9,4 +9,15 @@ public class MyController {
     public String showFIrstView() {
         return "first-view";
     }
+
+    @RequestMapping("/askDetails")
+    public String askEmployeeDetails() {
+        return "ask-emp-details-view";
+    }
+
+    @RequestMapping("/showDetails")
+    public String showEmployeeDetails() {
+        return "show-emp-details-view";
+
+    }
 }
