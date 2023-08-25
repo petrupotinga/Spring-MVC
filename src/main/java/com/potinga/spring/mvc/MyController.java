ba144ab549fe1537d048d6b73bcc0912de3bc78d
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-25 20:39:48 +0300

    adaugare directoriu

--- src/main/java/com/potinga/spring/mvc/MyController.java
+++ src/main/java/com/potinga/spring/mvc/MyController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MyController {
     @RequestMapping("/")
-    public String showFIrstView(){
+    public String showFIrstView() {
         return "first-view";
     }
 }
