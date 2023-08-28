c2571c30c3a3f21a98a91211cde69795b1c7e4bb
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-08-28 13:51:35 +0300

    Update MyController.java

--- src/main/java/com/potinga/spring/mvc/MyController.java
+++ src/main/java/com/potinga/spring/mvc/MyController.java
@@ -1,7 +1,11 @@
 package com.potinga.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MyController {
@@ -15,9 +19,26 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
-    @RequestMapping("/showDetails")
-    public String showEmployeeDetails() {
-        return "show-emp-details-view";
+//        @RequestMapping("/showDetails")
+//    public String showEmployeeDetails() {
+//        return "show-emp-details-view";
+//    }
 
-    } 
+
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails(HttpServletRequest request, Model model) {
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);
+//        return "show-emp-details-view";
+//    }
+
+    @RequestMapping("/showDetails")
+    public String showEmpDetails(@RequestParam("employeeName")
+                                 String empName, Model model) {
+        empName = "Mr. " + empName;
+        model.addAttribute("nameAttribute", empName);
+        return "show-emp-details-view";
+    }
+
 }
