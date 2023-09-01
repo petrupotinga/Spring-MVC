645d2a920df42c2d273e06ec4df2c40b5047457e
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-01 18:33:04 +0300

    Update Employee.java

--- src/main/java/com/potinga/spring/mvc/Employee.java
+++ src/main/java/com/potinga/spring/mvc/Employee.java
@@ -11,6 +11,7 @@ public class Employee {
     private Map<String, String> departments;
     private String carBrand;
     private Map<String, String> carBrands;
+    private String[] languages;
 
     public Employee() {
         departments = new HashMap<>();
@@ -78,6 +79,14 @@ public class Employee {
 
     public void setCarBrands(Map<String, String> carBrands) {
         this.carBrands = carBrands;
+    }
+
+    public String[] getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(String[] languages) {
+        this.languages = languages;
     }
 
     @Override
