32c3bae0afe94d4e9e146692c5919bf71e748e0d
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-04 10:33:19 +0300

    Update MyController.java

--- src/main/java/com/potinga/spring/mvc/MyController.java
+++ src/main/java/com/potinga/spring/mvc/MyController.java
@@ -2,11 +2,13 @@ package com.potinga.spring.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/employee")
@@ -23,16 +25,13 @@ public class MyController {
     }
 
     @RequestMapping("/showDetails")
-    public String showEmpDetails(@ModelAttribute("employee")
-                                 Employee emp) {
-        String name = emp.getName();
-        emp.setName("Mr." + name);
-        String surname = emp.getSurname();
-        emp.setSurname(surname + "!");
-        int salary = emp.getSalary();
-        emp.setSalary(salary*10);
-
-        return "show-emp-details-view";
+    public String showEmpDetails(@Valid @ModelAttribute("employee")
+                                 Employee emp, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "ask-emp-details-view";
+        } else {
+            return "show-emp-details-view";
+        }
     }
 
 //        @RequestMapping("/showDetails")
