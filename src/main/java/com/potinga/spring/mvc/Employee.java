52dbd1a403bbb6bdb411dc7813ee9390a2a73fc0
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-09-05 19:27:49 +0300

    Update Employee.java

--- src/main/java/com/potinga/spring/mvc/Employee.java
+++ src/main/java/com/potinga/spring/mvc/Employee.java
@@ -1,18 +1,17 @@
 package com.potinga.spring.mvc;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Employee {
-    @Size(min=2,message = "name must be min 2 sumbols")
+    @Size(min = 2, message = "name must be min 2 sumbols")
     private String name;
-//    @NotEmpty(message="surname is required field")
-    @NotBlank(message="surname is required field")
+    //    @NotEmpty(message="surname is required field")
+    @NotBlank(message = "surname is required field")
     private String surname;
+    @Min(value = 500, message = "must be greater than 499")
+    @Max(value = 1000, message = "must be less than 1001")
     private int salary;
     private String department;
     private Map<String, String> departments;
@@ -20,7 +19,8 @@ public class Employee {
     private Map<String, String> carBrands;
     private String[] languages;
     private Map<String, String> languageList;
-
+    @Pattern(regexp = "\\d{3}-\\d{2}-\\d{2}-\\d{2}", message="please use pattern XXX-XX-XX-XX")
+    private String phoneNumber;
 
     public Employee() {
         departments = new HashMap<>();
@@ -42,7 +42,6 @@ public class Employee {
 
 
     }
-
 
 
     public String getName() {
@@ -115,6 +114,14 @@ public class Employee {
 
     public void setLanguageList(Map<String, String> languageList) {
         this.languageList = languageList;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
     @Override
